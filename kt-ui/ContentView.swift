09ae24b9a8ef0632ee8de963de44dd7f416297d3
sunghwan.kwon <sunghwan.kwon@emotion.co.kr>
2026-05-04 15:27:54 +0900

    add: git init...

--- kt-ui/ContentView.swift
+++ kt-ui/ContentView.swift
@@ -6,14 +6,18 @@
 //
 
 import SwiftUI
+import SwiftUI
+import KTUIComponents
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Rectangle()
+                       .fill(Color.red500)
+            
+            Text("Hello, KT Design System!")
+                        .font(.headingLarge)
+                        .foregroundColor(.textPrimary)
         }
         .padding()
     }
